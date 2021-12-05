50518939e30c07699bd0412c713b0a4ff6bf7045
Rafaela Dartora <siglyane@gmail.com>
2021-12-04 21:08:46 -0300

    Cria metodo de criar usuarios

---
+++ src/controllers/userController.js
@@ -0,0 +1,19 @@
+const Users = require("../models/user");
+
+const creatUser = async (req, res) => {
+  try{
+    const user = await Users.create(req.body);
+    return res.status(200).json({
+      message: "Usuario cadastrado com sucesso",
+      user
+    })
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message
+    })
+  }
+}
+
+module.exports = {
+  creatUser
+}
