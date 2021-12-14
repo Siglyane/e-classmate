35ce1d50187311800fc2a79c97b8444bb6c9b475
Rafaela Dartora <siglyane@gmail.com>
2021-12-14 20:39:50 -0300

    Retorna os validators para o middlewares

---
+++ src/middlewares/validators/userValidator.js
@@ -0,0 +1,26 @@
+const { check, validationResult } = require("express-validator");
+
+const validateUser = [
+  check("name")
+    .trim()
+    .notEmpty()
+    .withMessage("Você deve informar seu nome"),
+  check("email")
+    .isEmail()
+    .trim()
+    .notEmpty()
+    .normalizeEmail()
+    .withMessage("Informe um e-mail válido"),
+  check("password")
+    .notEmpty()
+    .isStrongPassword()
+    .withMessage("Senha muito fraca"),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })};
+    next();
+    }
+  ];
+
+module.exports = validateUser
