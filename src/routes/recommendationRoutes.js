d686ab57bcff174d2c3a105e1f361cf2dcf7ab10
Rafaela Dartora <siglyane@gmail.com>
2021-12-16 21:22:08 -0300

    Altualiza documentação de rotas de recommendation

--- src/routes/recommendationRoutes.js
+++ src/routes/recommendationRoutes.js
@@ -5,15 +5,21 @@ const { checkAuth } = require("../middlewares/auth");
 const validator = require("../middlewares/validators/recommendationValidator")
 
 /*@router POST /recomm/create/:id
- *@desc Create a new recommendation based on id
+ *@desc Create a new recommendation based on user recommended's id
  *@access Private
  */
 router.post("/create/:id", checkAuth, validator, controller.createRecommendation);
 
-/*@router POST /recomm/create/:id
- *@desc Create a new recommendation based on id
+/*@router PATCH /recomm/update/:id
+ *@desc Update recommendation based on id
  *@access Private
  */
 router.patch("/update/:id", checkAuth, validator, controller.updateRecommendation);
+
+/*@router DELETE /recomm/delete/:id
+ *@desc Delete recommendation based on id
+ *@access Private
+ */
+router.delete("/delete/:id", checkAuth, controller.deleteRecommendation);
 
 module.exports = router;
